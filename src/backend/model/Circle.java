ca0da785457e69f08f176ae47d56654ea0e67a09
Franco Pampuri <francopampu05@gmail.com>
2024-12-01 20:55:08 -0300

    Backend with minor modifications and Frontend

---
+++ src/backend/model/Circle.java
@@ -0,0 +1,17 @@
+package backend.model;
+
+public class Circle extends Ellipse {
+
+    public Circle(Point centerPoint, double radius) {
+        super(centerPoint, radius, radius);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Círculo [Centro: %s, Radio: %.2f]", centerPoint, getRadius());
+    }
+
+    public double getRadius() {
+        return getsMinorAxis();
+    }
+}
---
+++ src/backend/model/Ellipse.java
@@ -0,0 +1,35 @@
+package backend.model;
+
+public class Ellipse extends Figure {
+
+    protected final Point centerPoint;
+    protected final double sMayorAxis, sMinorAxis;
+
+    public Ellipse(Point centerPoint, double sMayorAxis, double sMinorAxis) {
+        if (sMayorAxis <= 0 || sMinorAxis <= 0) {
+            throw new IllegalArgumentException("Axes must be positive values.");
+        }
+
+        this.centerPoint = centerPoint;
+        this.sMayorAxis = sMayorAxis;
+        this.sMinorAxis = sMinorAxis;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Elipse [Centro: %s, DMayor: %.2f, DMenor: %.2f]", centerPoint, sMayorAxis, sMinorAxis);
+    }
+
+    public Point getCenterPoint() {
+        return centerPoint;
+    }
+
+    public double getsMayorAxis() {
+        return sMayorAxis;
+    }
+
+    public double getsMinorAxis() {
+        return sMinorAxis;
+    }
+
+}
